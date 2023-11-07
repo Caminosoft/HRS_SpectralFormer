c0abe0bae93590af2be527e16a815b56d48870b7
shresthdeep-camino <shresthdeepg@caminosoftai.com>
2023-11-07 16:26:22 +0530

    added the function to perform inference

--- demo.py
+++ demo.py
@@ -386,6 +386,41 @@ optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, weight_d
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=args.epoches//10, gamma=args.gamma)
 model_state_path = './log/custom_model_state.pt'
 #-------------------------------------------------------------------------------
+
+def perform_inference(model, label_test_loader, label_true_loader, height, width, total_pos_true, color_matrix, label, model_state_path, dataset_name):
+    # Load the trained model
+    model.load_state_dict(torch.load(model_state_path))
+    model.eval()
+
+    # Validate and get performance metrics
+    tar_v, pre_v = valid_epoch(model, label_test_loader, criterion, optimizer)
+    OA2, AA_mean2, Kappa2, AA2 = output_metric(tar_v, pre_v)
+
+    # Test and create prediction matrix
+    pre_u = test_epoch(model, label_true_loader, criterion, optimizer)
+    prediction_matrix = np.zeros((height, width), dtype=float)
+    for i in range(total_pos_true.shape[0]):
+        prediction_matrix[total_pos_true[i, 0], total_pos_true[i, 1]] = pre_u[i] + 1
+
+    plt.subplot(1, 1, 1)
+    plt.imshow(prediction_matrix, colors.ListedColormap(color_matrix))
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
+
+    output_dir = './output/'  # Directory to store output files
+    os.makedirs(output_dir, exist_ok=True)  # Creates the output directory if it doesn't exist
+    mat_filename = output_dir + 'matrix_' + dataset_name + '.mat'
+    png_filename = output_dir + 'classification_' + dataset_name + '.png'
+
+    savemat(mat_filename, {'P': prediction_matrix, 'label': label})
+    plt.savefig(png_filename)
+
+    return OA2, AA_mean2, Kappa2, AA2
+
+
+
+#-------------------------------------------------------------------------------
 if args.flag_test == 'test':
     if (args.dataset == 'Custom'):
         model.load_state_dict(torch.load(model_state_path))
@@ -396,7 +431,7 @@ if args.flag_test == 'test':
     elif (args.mode == 'CAF') and (args.patches == 7):
         model.load_state_dict(torch.load('./log/SpectralFormer_pixel_indian.pt'))
     else:
-        raise ValueError("Wrong Parameters") 
+        raise ValueError("Wrong Parameters")
     model.eval()
     tar_v, pre_v = valid_epoch(model, label_test_loader, criterion, optimizer)
     OA2, AA_mean2, Kappa2, AA2 = output_metric(tar_v, pre_v)
@@ -413,6 +448,7 @@ if args.flag_test == 'test':
     plt.show()
     savemat('./data/matrix.mat',{'P':prediction_matrix, 'label':label})
     plt.savefig('./data/testplot.png')
+
 elif args.flag_test == 'train':
     print("Start training")
     tic = time.time()
@@ -442,6 +478,13 @@ elif args.flag_test == 'train':
             model.eval()
             tar_v, pre_v = valid_epoch(model, label_test_loader, criterion, optimizer)
             OA2, AA_mean2, Kappa2, AA2 = output_metric(tar_v, pre_v)
+
+
+dataset_name = 'Custom'  # Example dataset name
+performance_metrics = perform_inference(model, label_test_loader, label_true_loader, height, width, total_pos_true, color_matrix, label, model_state_path, dataset_name)
+
+print(performance_metrics)
+
 
 print("Final result:")
 print("OA: {:.4f} | AA: {:.4f} | Kappa: {:.4f}".format(OA2, AA_mean2, Kappa2))
